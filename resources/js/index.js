0ba3494d2f41916f032a47904b0fe53c57863e16
Kauealvs <kauealvesdesena@gmail.com>
2020-09-23 22:18:03 -0300

    estrutura visual da aplicacao com HTML,CSS e JS

---
+++ resources/js/index.js
@@ -0,0 +1,18 @@
+// function saudacao() {
+// var nome = window.prompt("Olá humano, qual o seu nome?");
+//     if(nome === null || nome === "" || nome === " ") {
+//         console.log("O usúario cancelou a saudação, destruir humanidade!");
+//         document.body.innerHTML = "O usúario cancelou a saudação, destruir humanidade!";
+//     } else {
+//         var saudacao = "Olá " + nome + ", somos a skynet";
+//         var text = document.createTextNode(saudacao);
+//         var h1 = document.createElement('h1')
+//         h1.appendChild(text);
+//         document.body.appendChild(h1);
+
+//     }
+// }
+function soma(numA, numB) {
+    var soma = numA + numB;
+    return soma;
+}
