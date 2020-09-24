cf4570f0d1fdd7b5d7d21a8c8d824647b80492f9
Kauealvs <kauealvesdesena@gmail.com>
2020-09-24 01:26:30 -0300

    criação de todas as funções de calculo no JS

--- resources/js/index.js
+++ resources/js/index.js
@@ -15,4 +15,86 @@
 function soma(numA, numB) {
     var soma = numA + numB;
     return soma;
+};
+
+function porcentagem(numA, numB) {
+    var porcentagemA = numA * (numB / 100);
+    var porcentagemA = porcentagemA + " %";
+    return porcentagemA;
+}
+
+function subtracao(numA, numB) {
+    var subtracao = numA - numB;
+    return subtracao;
+};
+
+function subtracao2(numA, numB) {
+    var subtracao = numB - numA;
+    return subtracao;
+};
+
+function multiplicacao(numA, numB) {
+    var multiplicacao = numA * numB;
+    return multiplicacao;
+};
+
+function divisao(numA, numB) {
+    var divisao = numA / numB;
+    if(divisao === 0) {
+        return "Divisão por zero";
+    } else {
+            return divisao;
+    }
+};
+
+function divisao2(numA, numB) {
+    var divisao = numB / numA;
+    if(divisao === 0) {
+        return "Divisão por zero";
+    } else {
+            return divisao;
+    }
+};
+
+//VOLTAR NESSA FUNÇÃO PARA SEPARAR A DE B
+function quadrado(numA) {
+    var quadradoA = Math.pow(numA, 2);
+    return quadradoA;    
+};
+
+function quadrado(numB) {
+    var quadradoB = Math.pow(numB, 2);
+    return quadradoB;    
+};
+
+function divisoresInteirosA(numA) {
+    var inteiros = [];
+    var contInt = 0;
+    for(var i = 1; i <= numA; i++) {
+        if(numA % i === 0) {
+            inteiros.push(i);
+            contInt = ++ contInt
+        }; 
+    };
+    return inteiros + "(" + contInt + " )";
+}
+
+function divisoresInteirosA(numB) {
+    var inteiros = [];
+    var contInt = 0;
+    for(var i = 1; i <= numB; i++) {
+        if(numB % i === 0) {
+            inteiros.push(i);
+            contInt = ++ contInt
+        }; 
+    };
+    return inteiros + "(" + contInt + " )";
+}
+
+function fatorial(numA) {
+    var fatorial = 1;
+    for(var i = 1; i <= numA; i++) {
+        fatorial = fatorial * i;
+    };
+    return fatorial;
 }
